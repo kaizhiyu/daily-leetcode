191cfefa7d3b390548761928856e44d8690d00bf
kaizhiyu <16031481+kaizhiyu@users.noreply.github.com>
2024-08-08 23:12:47 +0800

    使用bfs解决leetcode127

---
+++ src/main/java/org/dc/array/Leetcode127.java
@@ -0,0 +1,81 @@
+package org.dc.array;
+
+import java.util.*;
+
+/**
+ * 127. 单词接龙 【HARD】
+ *
+ * 字典 wordList 中从单词 beginWord 到 endWord 的 转换序列 是一个按下述规格形成的序列 beginWord -> s1 -> s2 -> ... -> sk：
+ * 每一对相邻的单词只差一个字母。
+ *  对于 1 <= i <= k 时，每个 si 都在 wordList 中。注意， beginWord 不需要在 wordList 中。
+ * sk == endWord
+ * 给你两个单词 beginWord 和 endWord 和一个字典 wordList ，返回 从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0 。
+ *
+ * 示例 1：
+ * 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ * 输出：5
+ * 解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+ *
+ * 示例 2：
+ * 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ * 输出：0
+ * 解释：endWord "cog" 不在字典中，所以无法进行转换。
+ *
+ * 提示：
+ * 1 <= beginWord.length <= 10
+ * endWord.length == beginWord.length
+ * 1 <= wordList.length <= 5000
+ * wordList[i].length == beginWord.length
+ * beginWord、endWord 和 wordList[i] 由小写英文字母组成
+ * beginWord != endWord
+ * wordList 中的所有字符串 互不相同
+ *
+ */
+public class Leetcode127 {
+    /**
+     * 这道题要解决两个问题：
+     *
+     * 图中的线是如何连在一起的
+     * 起点和终点的最短路径长度
+     * 首先题目中并没有给出点与点之间的连线，而是要我们自己去连，条件是字符只能差一个，所以判断点与点之间的关系，要自己判断是不是差一个字符，如果差一个字符，那就是有链接。
+     * 然后就是求起点和终点的最短路径长度，这里无向图求最短路，广搜最为合适，广搜只要搜到了终点，那么一定是最短的路径。因为广搜就是以起点中心向四周扩散的搜索。
+     * 本题如果用深搜，会非常麻烦。
+     *
+     * 另外需要有一个注意点：
+     * 本题是一个无向图，需要用标记位，标记着节点是否走过，否则就会死循环！
+     * 本题给出集合是数组型的，可以转成set结构，查找更快一些
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordList.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Map<String, Integer> map = new HashMap<>(); //记录单词对应路径长度
+        map.put(beginWord, 1);
+
+        while (!queue.isEmpty()) {
+            String word = queue.poll(); //取出队头单词
+            int path  = map.get(word); //获取到该单词的路径长度
+
+            for (int i = 0; i < word.length(); i++) {
+                char[] chars = word.toCharArray();
+                for (char k = 'a'; k <= 'z'; k++) {
+                    chars[i] = k;
+                    String newWord = String.valueOf(chars);
+                    if (newWord.equals(endWord)) {
+                        return path + 1;
+                    }
+                    if (wordSet.contains(newWord) && !map.containsKey(newWord)) {
+                        map.put(newWord, path + 1); //记录单词对应的路径长度
+                        queue.offer(newWord);//加入队尾
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
